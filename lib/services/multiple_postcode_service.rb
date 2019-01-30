30d2f3fbe98e784d0e4045a4abb9200d7839f4ea
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2019-01-30 09:48:31 +0000

    created a test for the multiple postcode

--- lib/services/multiple_postcode_service.rb
+++ lib/services/multiple_postcode_service.rb
@@ -12,16 +12,28 @@ class MultiplePostcodeService
     @multiple_postcode_data = JSON.parse(self.class.post('/postcodes', body: {"postcodes" => postcodes_array}).body)
   end
 
-  def retrieve_status_code 
-    @multiple_postcode_data['status']
+  def retrieve_results
+    @multiple_postcode_data['result']
   end
 
-  # def print_response
-  #   p @multiple_postcode_data
-  # end
+  def retrieve_array_postcode
+    retrieve_results[0]
+  end 
+
+  def retrieve_first_query
+    retrieve_array_postcode['query']
+  end
+
+  def retrieve_first_results
+    retrieve_array_postcode['result']
+  end
+
+  
+
+
 end
 
-# test = MultiplePostcodeService.new
-# # putting the postcodes inside the array returns multiple postcodes and all the details for each
-# test.multiple_postcode_request(["IG3 9JG", "M32 0JG", "NE30 1DP"])
-# test.print_response
+test = MultiplePostcodeService.new
+# putting the postcodes inside the array returns multiple postcodes and all the details for each
+test.multiple_postcode_request(["IG3 9JG", "M32 0JG", "NE30 1DP"])
+p test.retrieve_first_results
